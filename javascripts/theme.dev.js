068783a01782da84c17f170e8e550c0fecbb85fd
Amélie Husson <amelie@ameliehusson.com>
2012-08-28 16:46:16 +0200

    Custom scripts now load before bootstrap for better performances

--- javascripts/theme.dev.js
+++ javascripts/theme.dev.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 
     // Add scripts before closing body tag
-    $('body').append('<script src="/themes/ah/javascripts/bootstrap.min.js" type="text/javascript"></script>')
-        .append('<script src="/themes/ah/javascripts/custom.js" type="text/javascript"></script>');
+    $('body')
+        .append('<script src="/themes/ah/javascripts/custom.js" type="text/javascript"></script>') // Added before for better performances
+        .append('<script src="/themes/ah/javascripts/bootstrap.min.js" type="text/javascript"></script>');
 });
